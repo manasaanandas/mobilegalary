2fac37e8e749647f7708347d4c474f7c47718938
sai <anandas.manasa06@gmail.com>
2016-11-29 17:14:02 +0530

    This is my first commit

---
+++ src/main/java/mobilegalary/controller/HomeController.java
@@ -0,0 +1,17 @@
+package mobilegalary.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public ModelAndView Home()
+	{
+		return new ModelAndView("home");
+	}
+
+}
